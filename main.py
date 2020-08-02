b36b5f2a186080755064710717097b53d0b1ac8f
HUGUET <hug211mire@gmail.com>
2020-08-02 13:01:17 +0200

    add cli

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
-import json
+"""Lrtemplate 
 
-fp = open("test.json","r")
+Usage:
+    main.py <file>
+    main.py -h|--help
+    main.py -v|--version
 
-js = json.load(fp)
+Options:
+    <file>        Lightroom lrtemplate file 
+    -h --help     Show this screen.
+    -v --version  Show version.
+"""
 
-#print(type(js))
-#jss = js["value"]["settings"]
-#
-#for k in js["value"]["settings"]:
-#    print(k,":",jss[k])
+from docopt import docopt
+import convert_lr_to_json as cvj
 
+
+if __name__ == '__main__':
+
+    arguments = docopt(__doc__, version='v0.0.1')
+
+    __file = arguments["<file>"]
+
+    cvj.convert(__file,__file.split(".")[0]+".json")
+    print("\033[1;32;40m File converted with Success")
