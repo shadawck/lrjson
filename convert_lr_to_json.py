95c246c1009fe163aa91d521ef74303230fc0d83
HUGUET <hug211mire@gmail.com>
2020-08-02 13:00:55 +0200

    add conversion to function

--- convert_lr_to_json.py
+++ convert_lr_to_json.py
@@ -1,48 +1,55 @@
+
 import json
 import re
-from pprint import pprint
 
 # Required Transform 
 # 1 - Replace all "=" by ":"
 # 2 - Add quote on all keyword -> Add quote on all keyword preceded by ":"
 # 3 - Replace "{}" by "[]" if there is no a couple "key:value" in it.
-# 4 (Cleaning) - Remove "," after all closing "}"
-
-fin = open("forest_light_test.lrtemplate","r")
-fout = open("forest.json","w")
+# 4 (Cleaning) - Remove "," after all closing "}" or "]"
 
 
-# 1 - Replace all "=" by ":"
-lines = fin.readlines()
-fout.writelines(lines[0].split()[-1])
-lines.pop(0)
+def convert(infile,outfile):
+    
+    fin = open(infile,"r")
+    fout = open(outfile,"w")
 
-for line in lines:
 
     # 1 - Replace all "=" by ":"
-    line = line.replace("=",":") 
+    lines = fin.readlines()
+    fout.writelines(lines[0].split()[-1]) # replace "s:{" by "{"
+    lines.pop(0) # the first line has already been treated so remove it
 
-    # 2 - Add quote on all keyword -> Add quote on all keyword preceded by ":"
-    replace = re.findall(r"([a-zA-Z0-9]*)\s:",line)
-    if len(replace) < 1:
+    for line in lines:
+
+        # 1 - Replace all "=" by ":"
+        line = line.replace("=",":")
+
+        # 2 - Add quote on all keywords preceded by ":"
+        replace = re.findall(r"([a-zA-Z0-9]*)\s:",line)
+        if len(replace) < 1:
+            fout.writelines(line) 
+            continue
+        line = line.strip("\t").split()
+        line = "\"" + replace[0] + "\"" +  "".join(line[1::]) + "\n"
+
         fout.writelines(line) 
-        continue
-    line = line.strip("\t").split()
-    line = "\"" + replace[0] + "\"" +  "".join(line[1::]) + "\n"
 
+    fin.close()
+    fout.close()
 
-    fout.writelines(line) 
+    # 3 - Replace "{}" by "[]" if there is no a couple "key:value" in it.
+    fout = open(outfile,"rt")
+    fdata = fout.read()
+    fout.close()
+    
+    mod1 = re.sub(r"({)\n\t+","[",fdata)
+    mod2 = re.sub(r"[0-9]+,\n\t+(})","]",mod1)
 
-fin.close()
-fout.close()
+    # 4 (Cleaning) - Remove "," after all closing "}" or "]"
+    mod3 = re.sub(r"\,(?=\s*?[\}\]])","",mod2)
 
-fout = open("forest.json","rt").read()
-mod1 = re.sub(r"({)\n\t+","[",fout)         
-mod2 = re.sub(r"[0-9]+,\n\t+(})","]",mod1)   
-#mod = re.finditer(r"{\n\t+([0-9]+,\n\t+)+}",fout)
-mod3 = re.sub(r"\,(?=\s*?[\}\]])","",mod2)
-
-fout = open("final.json","w")
-fout.write(mod3)
-fout.close()
+    fout = open(outfile,"w")
+    fout.write(mod3)
+    fout.close()
 
